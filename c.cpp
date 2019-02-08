4990bd09d76b9e83869ecc73e75e8388e265e3bf
AnkitaKumari15 <43900609+AnkitaKumari15@users.noreply.github.com>
2019-02-08 21:37:39 +0530

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int l,b,area;
+cout<<"\n Enter length";
+cin>>l;
+cout<<"\n Enter breadth";
+cin>>b;
+area=l*b;
+cout<<"\narea"<<area;
+return 0;
+}
